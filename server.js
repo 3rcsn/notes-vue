b994fa259923729f819bf03de87d13805e9269dd
ercsn <33rcsn@gmail.com>
2026-06-22 21:54:45 -0300

    Mongoose conn

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
-const dotenv = require('dotenv');
+const {noteModel, noteSchema} = require('./models/note');
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const dotenv = require('dotenv');
 
+const app = express();
 app.use(express.json());
 
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT;
+const db = mongoose.connection;
+
+const autoIncrement = require('mongoose-sequence')(mongoose);
+
+noteSchema.plugin(autoIncrement, { inc_field: 'id' });
+
+app.get('/api/getNotes ', async (req, res) => {
+    try {
+        const notes = await noteModel.find();
+        res.json(notes);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.post('/api/notes', async (req, res) => {
+    try {
+        const newNote =  new noteModel({
+            name: req.body.name,
+            description: req.body.description,
+        });
+        await newNote.save();
+        res.status(201).send(newNote);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    noteModel.insertOne(req.body).then(note => {
+    });
+})
+
 app.listen(PORT, () => {
     console.log('express up');
 });
+
+
+
+
--- vue-project/src/components/Edit.vue
+++ vue-project/src/components/Edit.vue
@@ -1,12 +1,12 @@
 <script setup>
     import { ref } from 'vue';
-    const noteContent = ref('This is a sample note.');
+    const placeHolder = ref('This is a sample note.');
 </script>
 
 <template>
     <div class="edit-note">
         <div class="header">Edit Note</div>
-        <textarea id="newNote" v-model="noteContent" rows="10" cols="50"></textarea>
+        <textarea id="newNote" v-model="placeHolder" rows="10" cols="50"></textarea>
         <br>
         <button @click="saveNotes">Save</button>
     </div>
